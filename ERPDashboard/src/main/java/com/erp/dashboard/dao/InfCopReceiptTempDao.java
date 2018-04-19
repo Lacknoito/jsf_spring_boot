a6e5332349c96ed188293d7acc72a536eac94c54
NNoito <NNoito@THBKK01ITSN127.kerrylogistics.com>
2018-04-19 09:05:54 +0700

    จัดหน้าจอ

--- ERPDashboard/src/main/java/com/erp/dashboard/dao/InfCopReceiptTempDao.java
+++ ERPDashboard/src/main/java/com/erp/dashboard/dao/InfCopReceiptTempDao.java
@@ -39,6 +39,7 @@ public class InfCopReceiptTempDao implements IInfCopReceiptTempDao{
 		str.append(" 		and b.FLEX_VALUE_SET_ID = :valueSetId ");
 		str.append(" 	inner join FND_FLEX_VALUES_TL t on b.FLEX_VALUE_ID = t.FLEX_VALUE_ID ");
 		str.append(" 	where trunc(r.ar_receipt_date) = to_date(:date,'MM/DD/YYYY') ");
+		str.append(" 		and r.ar_amount_dis <> 0 ");
 		str.append(" 	group by r.ar_shop_name, r.ar_receipt_date, r.ar_amount_header, r.record_status ");
 		str.append(" ) receiptTemp ");
 		str.append(" group by receiptTemp.Branchtype ");
@@ -90,6 +91,7 @@ public class InfCopReceiptTempDao implements IInfCopReceiptTempDao{
 		str.append(" 		and r.ar_shop_name <> '181002' ");
 		str.append(" 		and r.ar_shop_name not like '182%' ");
 		str.append(" 		and r.ar_shop_name not like '19%' ");
+		str.append(" 		and r.ar_amount_dis <> 0 ");
 		str.append(" 	group by r.ar_shop_name, r.ar_receipt_date, r.ar_amount_header, r.record_status ");
 		str.append(" ) receiptTemp  ");
 		str.append(" group by receiptTemp.Branchtype ");
@@ -149,6 +151,7 @@ public class InfCopReceiptTempDao implements IInfCopReceiptTempDao{
 		str.append(" 	and b.FLEX_VALUE_SET_ID = :valueSetId ");
 		str.append(" inner join FND_FLEX_VALUES_TL t on b.FLEX_VALUE_ID = t.FLEX_VALUE_ID  ");
 		str.append(" where trunc(r.ar_receipt_date) = to_date(:date,'MM/DD/YYYY')  ");
+		str.append(" 	and r.ar_amount_dis <> 0 ");
 		
 		param.put("valueSetId", ERPUtils.FIX_FLEX_VALUE_SET_ID);
 		param.put("date", ERPUtils.convertDateToStringFormat(date, "MM/dd/YYYY"));
---
+++ ERPDashboard/src/main/webapp/js/erp.js
@@ -0,0 +1,30 @@
+function showModal() {
+	$('#myModal').modal();
+}
+
+function closeModal(){
+	$('#myModal').modal('hide');
+	
+	/*$('html, body').animate({
+        scrollTop: $("#chartdiv1").offset().top
+    }, 800);*/
+	
+	clearDivChart();
+}
+
+function statusChange(obj, param){
+	document.getElementById("formId:x").value = param;
+	document.getElementById("formId:y").value = $(obj).prop("checked");
+	
+	updateStatus();
+}
+function showModalPDF() {
+	$('#myModalPDF').modal();
+}
+function clearDivChart(){
+	$("#divChart1").removeClass("col-sm-6");
+	$("#divChart1").addClass("col-sm-11");
+	
+	$("#divChart2").removeClass("col-sm-6");
+	$("#divChart2").addClass("col-sm-1");
+}
