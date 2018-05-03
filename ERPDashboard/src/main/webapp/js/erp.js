ac1c25f5c7a92964518c91868d569d935765015e
NNoito <NNoito@THBKK01ITSN127.kerrylogistics.com>
2018-05-03 15:24:28 +0700

    s

--- ERPDashboard/src/main/webapp/js/erp.js
+++ ERPDashboard/src/main/webapp/js/erp.js
@@ -37,3 +37,66 @@ function onStartProcess() {
 function onEndProcess() {
 	$('.modalLoading').modal('hide');
 }
+
+function handleLegend1Click(graph) {
+	document.getElementById("formId:x").value = graph.title;
+	
+	if(graph.title == 'BSD'
+  		|| graph.title == 'FC/KE'
+  		|| graph.title == 'UPC'){
+  		selectItemFromChart();
+  	}else{
+  		selectItemFromChartPS();
+  		
+  		$("#divChart1").removeClass("col-sm-11");
+  		$("#divChart1").addClass("col-sm-6");
+  		
+  		$("#divChart2").removeClass("col-sm-1");
+  		$("#divChart2").addClass("col-sm-6");
+  		
+  		$("#ExportDCSP").show();
+  	}
+}
+function handleLegend2Click(graph) {
+	document.getElementById("formId:x").value = graph.title;
+	selectItemFromChart();
+}
+
+
+function keyPressDecimalOnly(myfield, e, dec)
+{
+    var key;
+    var keychar;
+    
+    if (window.event) key = window.event.keyCode;
+    else if (e) key = e.which;
+    else return true;
+    keychar = String.fromCharCode(key);
+
+    if ((key==null) || (key==0) || (key==8) || 
+        (key==9) || (key==13) || (key==27) ) {
+       return true;
+
+    } else if ((("0123456789.").indexOf(keychar) > -1)) {
+    	if (keychar == "." && myfield.value.indexOf(".") > -1) {
+    		return false; 
+    	} else {
+       		return true;
+       	}
+
+    } else if (dec && (keychar == ".")) {
+	    myfield.form.elements[dec].focus();
+	    return false;
+    } else return false;
+}
+
+function converDecimalFormat(obj) {
+	if(obj.value) {
+		if(obj.value.length == 1
+				&& obj.value == '.'){
+			obj.value = '0';
+		}
+		var input = parseFloat(obj.value);
+    	obj.value = input.toFixed(2);
+	}
+}
